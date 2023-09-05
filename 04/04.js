a90cb19c8b5889d7ec0e62050d494d25df885dbe
color7921 <132988693+color7921@users.noreply.github.com>
2023-09-05 12:59:52 +0900

    dice_javascript
    - 주사위 굴리는 프로그램을 javascript로 만들기

--- 04/04.js
+++ 04/04.js
@@ -1,7 +1,40 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
+//     const bts = document.querySelectorAll("button") ;
+//     //버튼 가져오기
+//     bts.forEach((item) =>{
+//         item.addEventListener("click", ()=>{
+//             dice2(parseInt(item.textContent));
+//         });
+//     });
+//     //각 버튼에 이벤트 달기
+
+
+//     document.querySelector("#bt1")
+// //돔 완성되면 호출
+
+const bt = document.querySelector("button");
+const radios = document.querySelectorAll("input[type=radio]");
+bt.addEventListener("click", ()=>{
+    for(let item of radios) {
+        if (item.checked){
+            console.log(item.value);
+            dice2(parseInt(item.value));
+            //문자를 숫자로 변환
+            break;
+            //foreach는 break 안됨
+        }
+    }
+    //console.log(radios)
+    //console.log(item)
+    
+});
+//확인 버튼 가져오기
+
+
+
 
 });
-
+//주사위 보기
 const dice = () => {
     const adiv = document.querySelector(".adiv");
     
@@ -18,3 +51,29 @@ const dice = () => {
 
     adiv.innerHTML = `<img src='./image/${n}.png'>`;
 }
+
+// 버튼 클릭시 주사위 보기
+const dice2 = (seln) => {
+    //함수 할당, 화살표 함수 == function() {} <<<--- 익명함수
+    //주사위 숫자 1~6
+    let n = Math.floor(Math.random() * 6) + 1;
+    
+    //주사위 이미지 넣을 위치
+    //const adiv = document.getElementById("adiv");
+    const adiv = document.querySelector("#adiv");
+    adiv.innerHTML = `<img src='./image/${n}.png'>`;
+    //변수를 집어 넣어서 
+
+    
+    const h2 = document.querySelector("hgroup > h2");
+    h2.style.color = "red";
+    //결과 출력을 위한 위치
+    if ( n === seln) h2.textContent = "win";
+    
+    else h2.textContent = "lose";
+} 
+
+
+
+
+
