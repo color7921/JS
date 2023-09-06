7f66c53129119373121e7520135948ac063bad85
color7921 <132988693+color7921@users.noreply.github.com>
2023-09-06 14:12:36 +0900

    섭씨온도 / 화씨온도 변환
    - 섭씨, 화씨 온도 변환하는 프로그램 작성

---
+++ 05/05.js
@@ -0,0 +1,76 @@
+const toggle = (v1, v2, v3, v4) => {
+    txt1.value = "";
+    txt2.value = "";
+    sel1.value = v1;
+    sel2.value = v2;
+    div1.textContent = v3;
+    div2.textContent = v4;
+}
+
+//섭씨온도 화씨온도
+const cTof = (c) => {
+    txt2.value = (c * 9/5) + 32;
+
+}
+
+//화씨온도 섭씨온도
+const fToc = (f) => {
+    txt2.value = (f - 32) * 5/9;
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sel1 = document.querySelector("#sel1");
+    const sel2 = document.querySelector("#sel2");
+
+    const div1 = document.querySelector("#div1");
+    const div2 = document.querySelector("#div2");
+
+    const txt1 = document.querySelector("#txt1");
+    const txt2 = document.querySelector("#txt2");
+
+
+    //온도 입력
+    txt1.addEventListener("input", () => {
+        if (sel1.value === "C") cTof(parseFloat(txt1.value))  ;
+        else fToc(parseFloat(txt1.value));
+         
+    })
+    //select box1 제어
+    sel1.addEventListener("change", ()=>{
+        //console.log(sel1.value);
+        if (sel1.value === "C"){
+            toggle("C", "F", "℃", "℉");
+        }
+            // sel2.value = "F";
+            // div1.textContent = "℃";
+            // div2.textContent = "℉";
+            // 토글키랑 같은말
+        
+        else {
+            toggle("F", "C", "℉", "℃");
+        }
+            // sel2.value = "C";
+            // div1.textContent = "℉";
+            // div2.textContent = "℃";
+        
+    });
+    sel2.addEventListener("change", ()=>{
+        //console.log(sel2.value);
+        if (sel1.value === "C"){
+            toggle("F", "C", "℉", "℃");
+        }
+            // sel2.value = "F";
+            // div1.textContent = "℃";
+            // div2.textContent = "℉";
+        
+        else {
+            toggle("C", "F", "℃", "℉");
+        }
+            // sel2.value = "C";
+            // div1.textContent = "℉";
+            // div2.textContent = "℃";
+    
+    });
+    
+});
